cc45e00a04eb05c4bbd0c20c3c2130a6c447575b
Marcelo Rangel Barros <marcelorangelb2006@gmail.com>
2025-07-21 21:50:29 -0300

    Atualizando os códigos

--- nivel2/carro.ts
+++ nivel2/carro.ts
@@ -1,38 +1,13 @@
-// Classe base
-class Carro {
-  constructor(
-    public marca: string,
-    public modelo: string,
-    public ano: number
-  ) {}
-
-  obterDetalhes(): string {
-    return `${this.marca} ${this.modelo}, ano ${this.ano}`;
-  }
+export class Carro {
+    constructor (
+        public marca: String,
+        public modelo: String,
+        public ano: number
+    ) {}
+    obterDetalhes(): String{
+        return `Marca: ${this.marca}, Modelo: ${this.modelo}, Ano: ${this.ano}`;
+    }
 }
 
-// Instância da classe Carro
-const carro = new Carro("Toyota", "Corolla", 2022);
-console.log("Detalhes do carro:", carro.obterDetalhes());
-
-
-// Classe derivada com herança
-class CarroEletrico extends Carro {
-  constructor(
-    marca: string,
-    modelo: string,
-    ano: number,
-    public autonomiaBateria: number
-  ) {
-    super(marca, modelo, ano);
-  }
-
-  // Sobrescrevendo o método
-  obterDetalhes(): string {
-    return `${super.obterDetalhes()}, autonomia: ${this.autonomiaBateria} km`;
-  }
-}
-
-// Instância da classe CarroEletrico
-const tesla = new CarroEletrico("Tesla", "Model 3", 2023, 500);
-console.log("Detalhes do carro elétrico:", tesla.obterDetalhes());
+const carro = new Carro("Volkswagen", "Fusca", 1975);
+console.log("Carro Cadastrado!", carro.obterDetalhes())
